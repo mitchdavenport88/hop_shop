2ffdc63796c5964c84cb42fc3c8a68a981bab6a3
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-10-18 10:51:52 +0000

    Add products app and create product models

---
+++ products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Category, Country, Product
+
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(Product)
---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=60)
+    friendly_name = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Country(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+    name = models.CharField(max_length=60)
+    friendly_name = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    sku = models.CharField(max_length=12, null=True, blank=True)
+    description = models.TextField()
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    country = models.ForeignKey('Country', null=True, blank=True,
+                                on_delete=models.SET_NULL)
+    abv = models.DecimalField(max_digits=4, decimal_places=2)
+    size = models.IntegerField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
