7abaa5d612b24a75f607b5a11de286b91444b749
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-10-18 12:26:25 +0000

    Customise admin panel. Add products view and temp

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Category, Country, Product
 
-admin.site.register(Category)
-admin.site.register(Country)
-admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'country',
+        'category',
+        'price',
+        'image',
+    )
+
+    # ordering = ('sku',) - uncomment once sku's are added
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Product, ProductAdmin)
