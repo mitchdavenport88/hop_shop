3d56d7bd1f8192ac4fe62edc8c1f557e6a2210e0
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-10-20 09:23:42 +0000

    Alter navbar links. Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def all_products(request):
     """ A view to show all products """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query) | Q(
+                    country__name__icontains=query) | Q(
+                        category__friendly_name__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_query': query,
     }
 
     return render(request, 'products/products.html', context)
