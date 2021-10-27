adb9661679c96e1fb88b8e203b8cbd56e4f0c5a8
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-10-27 15:49:31 +0000

    Add forms, admin and signals to checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'street_address',
+                  'town_or_city', 'county', 'postcode', 'country',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Set auto focus to the first field, remove auto-generated
+        input labels and insert placeholders to replace them
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address': 'Address',
+            'town_or_city': 'Town or City',
+            'county': 'County or State',
+            'postcode': 'Postcode',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field] *}'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
