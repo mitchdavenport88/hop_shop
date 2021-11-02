8edb0e512ff97085970b351cae65282222be98f6
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-11-02 17:36:38 +0000

    Update and style allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """ Display profile (user specific) """
-
-    context = {}
+    get_user_profile = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        'profile': get_user_profile,
+    }
 
     return render(request, 'profiles/profile.html', context)
