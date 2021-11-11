e4c2b441b728b78828cd4c767752c3eb691f831b
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-11-11 18:57:12 +0000

    Style blog comments section

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,15 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set auto focus to comment input field, remove
+        auto-generated input labels and insert placeholder
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['body'].widget.attrs['autofocus'] = True
+        self.fields['body'].widget.attrs['placeholder'] = (
+            'Write your comment here...')
+        self.fields['body'].label = False
+        self.fields['body'].widget.attrs['class'] = 'comment-form-input'
