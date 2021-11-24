73815db6ed4b8bc7115418faae0c7a8963a53d8d
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-11-24 11:46:45 +0000

    Amend home view to get user_profile variable so wishlist icon toggles

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 def index(request):
     """ A view to return the index/home page """
     products = Product.objects.all().order_by('-pk')[0:4]
 
-    return render(request, 'home/index.html', {'products': products})
+    if request.user.is_authenticated:
+        user_profile = get_object_or_404(UserProfile, user=request.user)
+    else:
+        user_profile = None
+
+    context = {
+        'products': products,
+        'user_profile': user_profile,
+    }
+
+    return render(request, 'home/index.html', context)
