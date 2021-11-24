67690db4ae161c0360c8e8747d22bf95809a6026
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-11-24 11:19:01 +0000

    Add new arrivals section to index.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from products.models import Product
+
 
 def index(request):
     """ A view to return the index/home page """
+    products = Product.objects.all().order_by('-pk')[0:4]
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {'products': products})
